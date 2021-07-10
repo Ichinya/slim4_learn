0e2b4779ab6c2a82a16ce0fddb10e4655c599f8b
Ichi <ichi-perm@yandex.ru>
2021-07-10 23:41:50 +0500

    изменен роутинг маршрутов и добавлен первый контроллер

--- app/routes.php
+++ app/routes.php
@@ -2,23 +2,22 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Http\Controllers\WelcomeController;
 
 return function (Slim\App $app) {
 
 
-    $app->get('/home', function (Request $request, Response $response, $args) {
-        $name = 'Slim';
-        return view($response, 'auth.home', compact('name'));
-    });
+//    $app->get('/home', function (Request $request, Response $response, $args) {
+//        $name = 'Slim';
+//        return view($response, 'auth.home', compact('name'));
+//    });
 
-    $app->get('/', function (Request $request, Response $response, $args) {
-        $response->getBody()->write("Hello world!");
-        return $response;
-    });
+    $app->get('/', [WelcomeController::class, 'index']);
+    $app->get('/{name}', [WelcomeController::class, 'show']);
 
-    $app->get('/test', function (Request $request, Response $response, $args) {
-        $response->getBody()->write("Hello test world!");
-        return $response;
-    });
+//    $app->get('/test', function (Request $request, Response $response, $args) {
+//        $response->getBody()->write("Hello test world!");
+//        return $response;
+//    });
 
 };
